1ba8612a9fe5441894537436371d35c25576b497
aatosls <aatos.latva-somppi@helsinki.fi>
2022-04-06 15:21:38 +0300

    invoke tasks

---
+++ kare/tasks.py
@@ -0,0 +1,13 @@
+from invoke import task
+
+@task
+def start(ctx):
+    ctx.run("python3 src/index.py", pty=True)
+
+@task
+def test(ctx):
+    ctx.run("pytest src")
+
+@task
+def coverage_report(ctx):
+    ctx.run("coverage run --branch -m pytest src\ncoverage html")
